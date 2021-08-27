78cddbfc0dd5d3e90e8b970c99d7906d5705cfdb
ciamac-da <ciamac.davoudi@gmail.com>
2021-08-27 09:11:51 +0200

    Verified by Marti4n v1.0.4 showing score

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const form = document.querySelector(".quiz-form")
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
 
-const correctAnswers = ["D", "A", "B"]
+const correctAnswers = ["D", "A", "B"];
 
-form.addEventListener("submit", e => {
-    e.preventDefault()
-    let score = 0
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value]
-    userAnswers.forEach((answer, index) => {
-        if (answer === correctAnswers[index]) {
-            score += 10
-        }
-        console.log(score)
-        })
-        })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let score = 0;
+  const halfScore = 50
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value];
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 10;
+    }
+  })
+  result.querySelector("span").textContent = `${score}%`
+  result.classList.remove("d-none")
+  result.querySelector("span").style.color = `${ score >= halfScore ? "green" : "red"}`
+})
+
